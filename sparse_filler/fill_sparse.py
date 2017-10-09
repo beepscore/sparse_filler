eee7b6025ca4cb01c70bf2ec9506c4edd7c5e2ec
beepscore <support@beepscore.com>
2017-10-09 16:16:27 -0700

    write output file

--- sparse_filler/fill_sparse.py
+++ sparse_filler/fill_sparse.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
 
 
-def write_filled(input_filename):
+def write_filled(input_filename, output_filename):
     # https://stackoverflow.com/questions/8009882/how-to-read-large-file-line-by-line-in-python#8010133
 
-    with open(input_filename) as lines:
-        for line in lines:
-            print(line)
+    with open(output_filename, 'w') as output_file:
+
+        with open(input_filename) as lines:
+            for line in lines:
+                output_file.write(line[0:7])
+                output_file.write('\n')
 
 
-write_filled("../data/input.txt")
+input_filename = "../data/input.txt"
+output_filename = "../data/output.txt"
+write_filled(input_filename, output_filename)
