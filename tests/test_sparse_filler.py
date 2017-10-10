33b099081aeb1fca821630c6481ae0582221cccf
beepscore <support@beepscore.com>
2017-10-10 09:11:38 -0700

    Add test_int_after_colon_no_colon

--- tests/test_sparse_filler.py
+++ tests/test_sparse_filler.py
@@ -20,6 +20,9 @@ class TestSparseFiller(unittest.TestCase):
     def test_int_after_colon(self):
         self.assertEqual(fill_sparse.int_after_colon('5:1'), 1)
 
+    def test_int_after_colon_no_colon(self):
+        self.assertEqual(fill_sparse.int_after_colon('5'), None)
+
     def test_filled(self):
         self.assertEqual(fill_sparse.filled('1 2:1 3:1 7:1 10:1'), [1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 1])
         self.assertEqual(fill_sparse.filled('2 5:1 8:1'), [2, 0, 0, 0, 0, 1, 0, 0, 1])
