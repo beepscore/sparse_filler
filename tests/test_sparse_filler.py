ed2cf94c3a3864ac4b5f3f93440da1a32546cc86
beepscore <support@beepscore.com>
2017-10-09 20:48:09 -0700

    Add test_int_before_colon

--- tests/test_sparse_filler.py
+++ tests/test_sparse_filler.py
@@ -5,9 +5,10 @@ from sparse_filler import fill_sparse
 class TestSparseFiller(unittest.TestCase):
 
     def test_line_list(self):
-        # call method under test
         self.assertEqual(fill_sparse.line_list('2 5:1 8:1'), ['2', '5:1', '8:1'])
 
     def test_int_element_zero(self):
-        # call method under test
         self.assertEqual(fill_sparse.int_element_zero(['2', '5:1', '8:1']), 2)
+
+    def test_int_before_colon(self):
+        self.assertEqual(fill_sparse.int_before_colon('5:1'), 5)
