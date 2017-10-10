0ada3e7777be61c6542aecbd5c19bc407cc0004c
beepscore <support@beepscore.com>
2017-10-09 20:46:44 -0700

    In test_sparse_filler fix import, add test_int_element_zero

--- tests/test_sparse_filler.py
+++ tests/test_sparse_filler.py
@@ -1,10 +1,13 @@
 import unittest
-from sparse_filler import line_list
+from sparse_filler import fill_sparse
 
 
 class TestSparseFiller(unittest.TestCase):
 
     def test_line_list(self):
         # call method under test
-        self.assertEqual(line_list('2 5:1 8:1'), ['2', '5:1', '8:1'])
+        self.assertEqual(fill_sparse.line_list('2 5:1 8:1'), ['2', '5:1', '8:1'])
 
+    def test_int_element_zero(self):
+        # call method under test
+        self.assertEqual(fill_sparse.int_element_zero(['2', '5:1', '8:1']), 2)
